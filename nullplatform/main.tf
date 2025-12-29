e65d61a132e11dfa666aa32862ec8f48bf0cd47f
Javi <javier.solis@nullplatform.io>
2025-12-29 08:40:50 -0300

    feat: endpoint exposer and related channel creation

--- nullplatform/main.tf
+++ nullplatform/main.tf
@@ -40,13 +40,40 @@ module "dimensions" {
 }
 
 module "endpoint_exposer_service_definition" {
-  source = "git::https://github.com/nullplatform/tofu-modules.git//nullplatform/service_definition?ref=feature/endpoint-exposer"
-  nrn          = var.nrn
-  np_api_key   = var.np_api_key
-  git_repo = "nullplatform/services"
-  git_ref      = "feat/endpoint-exposer"
-  git_service_path = "endpoint-exposer/entrypoint"
-  service_name        = "Endpoint Exposer 2"
+  source           = "git::https://github.com/nullplatform/tofu-modules.git//nullplatform/service_definition?ref=feature/endpoint-exposer"
+  nrn              = var.nrn
+  np_api_key       = var.np_api_key
+  git_repo         = "nullplatform/services"
+  git_ref          = "feat/endpoint-exposer"
+  git_service_path = "endpoint-exposer"
+  service_name     = "Endpoint Exposer Javi"
+
   service_description = "Endpoint Exposer Service to expose Endpoints"
+  use_tpl_files       = true # Set to true if using .tpl files, false for .json files
+  tags_selectors = {
+    "environment" = "production"
+    "region"      = "us-east-1"
+  }
 }
 
+module "endpoint_exposer_service_definition_agent_association" {
+  source                   = "git::https://github.com/nullplatform/tofu-modules.git//nullplatform/service_definition_agent_association?ref=feature/endpoint-exposer"
+  nrn                      = var.nrn
+  service_path             = "/root/.np/nullplatform/services/endpoint-exposer"
+  service_slug             = module.endpoint_exposer_service_definition.service_specification_slug
+  service_specification_id = module.endpoint_exposer_service_definition.service_specification_id
+  np_api_key               = var.np_api_key
+  tags_selectors = {
+    "environment" = "development"
+  }
+  agent_command = {
+    type = "exec"
+    data = {
+      cmdline = "/root/.np/nullplatform/services/endpoint-exposer/entrypoint/entrypoint",
+      environment = {
+        NP_ACTION_CONTEXT = "'$${NOTIFICATION_CONTEXT}'"
+        INGRESS_TYPE      = "istio"
+      }
+    } 
+  }
+}
